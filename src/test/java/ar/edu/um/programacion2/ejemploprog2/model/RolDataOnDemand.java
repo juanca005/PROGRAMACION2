2affdbd028e06842dd4cb5618442062e205bd19a
juanca005 <alessi.juan@gmail.com>
2016-02-16 19:13:15 -0300

    se agrego la consulta sql que valida solo a la tabla usuario

---
+++ src/test/java/ar/edu/um/programacion2/ejemploprog2/model/RolDataOnDemand.java
@@ -0,0 +1,6 @@
+package ar.edu.um.programacion2.ejemploprog2.model;
+import org.springframework.roo.addon.dod.RooDataOnDemand;
+
+@RooDataOnDemand(entity = Rol.class)
+public class RolDataOnDemand {
+}
